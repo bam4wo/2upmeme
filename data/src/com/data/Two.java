f5cce88ff42b3311ccd369328077a0663c355d95
bam4wo <43362974+bam4wo@users.noreply.github.com>
2019-09-21 14:40:45 +0800

    continue

--- data/src/com/data/Two.java
+++ data/src/com/data/Two.java
@@ -1,10 +1,31 @@
 package com.data;
 
-public class Two {
+import java.util.Scanner;
 
+public class Two {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] a = {5, 7, 4, 3, 9};
+		//search(a);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter number");
+		int x = scanner.nextInt();
+		int location = 1;
+		while(location <= a.length && a[location] != x) {
+			System.out.println(location++);
+			
+		}
+		if(location > a.length) {
+			System.out.println(location = 0);
+		}
 
 	}
-
+	/*
+	public static void search(int[] a) {
+		int location = 1;
+		while(location <= a.length && a[location] != x) {
+			
+		}
+		
+	}*/
 }
