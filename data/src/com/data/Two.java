af1ba5962c41775ea80013fb489f91367d3cb143
bam4wo <43362974+bam4wo@users.noreply.github.com>
2019-09-21 18:35:21 +0800

    資料結構與演算法-循序搜尋法

--- data/src/com/data/Two.java
+++ data/src/com/data/Two.java
@@ -6,26 +6,17 @@ public class Two {
 	
 	public static void main(String[] args) {
 		int[] a = {5, 7, 4, 3, 9};
-		//search(a);
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please enter number");
 		int x = scanner.nextInt();
 		int location = 1;
-		while(location <= a.length && a[location] != x) {
-			System.out.println(location++);
-			
+		while(location <= a.length && a[location - 1] != x) {
+			location++;	
 		}
 		if(location > a.length) {
-			System.out.println(location = 0);
+			location = 0;
 		}
+		System.out.println(location);
 
 	}
-	/*
-	public static void search(int[] a) {
-		int location = 1;
-		while(location <= a.length && a[location] != x) {
-			
-		}
-		
-	}*/
 }
